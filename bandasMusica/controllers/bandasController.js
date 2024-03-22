a44f6cbc2b51876c4954305d8b6286af6f5d1692
camuxsel <cselvas@udesa.edu.ar>
2024-03-22 15:16:09 -0300

    avance con controller

--- bandasMusica/controllers/bandasController.js
+++ bandasMusica/controllers/bandasController.js
@@ -1,1 +1,15 @@
-let bandasController = {};
+let bandas = require('../db/index');
+
+let listaBandas = bandas.lista;
+
+let bandasController = {listadoBanda : function (req, res) {
+    for (let i = 0; i < listaBandas.length; i++) {
+        let banda = listaBandas[i];
+    }
+    }, 
+    detalleBanda : function (req, res) {
+        
+    },
+};
+
+module.exports = bandasController;
--- bandasMusica/routes/index.js
+++ bandasMusica/routes/index.js
@@ -1,8 +1,11 @@
 var express = require('express');
 var router = express.Router();
 
+const bandasController = require('../controllers/bandasController');
+
 /* GET home page. */
-router.get('/', );
+router.get('/', bandasController.listadoBanda);
+
 
 
 module.exports = router;

96178c856634bc97e3f63dd6c0d15a632a23aea0
camuxsel <cselvas@udesa.edu.ar>
2024-03-23 17:58:39 -0300

    avance un poco

--- bandasMusica/controllers/bandasController.js
+++ bandasMusica/controllers/bandasController.js
@@ -1,15 +1,22 @@
-let bandas = require('../db/index');
+let bandas = require('../db');
 
 let listaBandas = bandas.lista;
 
 let bandasController = {listadoBanda : function (req, res) {
+    resultados = [];
     for (let i = 0; i < listaBandas.length; i++) {
-        let banda = listaBandas[i];
+        let infoBanda = listaBandas[i];
+        let banda = {nombre : infoBanda.nombre, cover : infoBanda.cover, id : infoBanda.id}
+        resultados.push(banda);
     }
+    res.render('index', {resultados : resultados})    
     }, 
     detalleBanda : function (req, res) {
         
     },
+    porGenero : function (req, res) {
+        
+    }
 };
 
 module.exports = bandasController;
--- bandasMusica/routes/index.js
+++ bandasMusica/routes/index.js
@@ -6,6 +6,10 @@ const bandasController = require('../controllers/bandasController');
 /* GET home page. */
 router.get('/', bandasController.listadoBanda);
 
+router.get('/id/:id', bandasController.detalleBanda);
+
+router.get('/genero/:genero', bandasController.porGenero);
+
 
 
 module.exports = router;
